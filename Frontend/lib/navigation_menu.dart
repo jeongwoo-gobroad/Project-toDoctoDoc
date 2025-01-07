97993c6e8d2f3672990a004fc9866b83bb5ef756
Jeongwoo Kim <jeongwoo_gobroad@gmail.com>
2025-01-07 21:01:23 +0900

    0.9.4

--- Frontend/lib/navigation_menu.dart
+++ Frontend/lib/navigation_menu.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:to_doc/ai_chat_main.dart';
 import 'package:to_doc/home.dart';
 import 'package:to_doc/screens/graph_test.dart';
 import 'package:to_doc/screens/map_screen.dart';
@@ -40,6 +41,6 @@ class NavigationMenu extends StatelessWidget {
 class NavigationController extends GetxController{
   final Rx<int> selectedIndex = 0.obs; //home 상태관리
 
-  final screens = [Home(), Container(color: Colors.green), GraphBoard() , MapAndListScreen(), Container(color: Colors.purple)]; 
+  final screens = [Home(), AichatMain(), GraphBoardTest() , MapAndListScreen(), Container(color: Colors.purple)];
 
 }
