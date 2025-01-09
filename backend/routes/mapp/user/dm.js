ffd9be5d1691ae4685ce8dfbce632f177441b385
Jeongwoo Kim <jeongwoo_gobroad@gmail.com>
2025-01-09 21:14:07 +0900

    0.9.4

--- backend/routes/mapp/user/dm.js
+++ backend/routes/mapp/user/dm.js
@@ -12,6 +12,36 @@ const Doctor = require("../../../models/Doctor");
 const Chat = require("../../../models/Chat");
 const User = mongoose.model("User", UserSchema);
 
+router.get(["/dm/list"], 
+    checkIfLoggedIn,
+    ifPremiumThenProceed,
+    async (req, res, next) => {
+        const user = await getTokenInformation(req, res);
+
+        try {
+            const usr = await User.findById(user.userid).populate(
+                [
+                    {path: 'chats', populate: {path: 'doctor', select: 'name'}}
+                ]
+            );
+
+            const chats = usr.chats;
+
+            if (chats.length > 0) {
+                res.status(200).json(returnResponse(false, "returnedChatList", chats));
+    
+                return;
+            } else {
+                res.status(201).json(returnResponse(false, "emptyChatList", "-"));
+    
+                return;
+            }
+        } catch (error) {
+            res.status(403).json(returnResponse(true, "errorAtDMLists", "-"));
+        }
+    }
+);
+
 router.get(["/dm"],
     checkIfLoggedIn,
     ifPremiumThenProceed,
@@ -36,6 +66,12 @@ router.get(["/dm"],
                 const newChat = await Chat.create({
                     user: uid,
                     doctor: did,
+                });
+                await User.findByIdAndUpdate(uid, {
+                    $push: {chats: newChat._id}
+                });
+                await Doctor.findByIdAndUpdate(did, {
+                    $push: {chats: newChat._id}``
                 });
 
                 res.status(200).json(returnResponse(false, "returnedNewChatID", {chatid: newChat._id}));
@@ -76,6 +112,48 @@ router.get(["/dm/:id"],
             return;
         } catch (error) {
             res.status(403).json(returnResponse(true, "errorAtDMDetailsReturn", "-"));
+
+            return;
+        }
+    }
+);
+
+router.delete(["/dm/:id"],
+    checkIfLoggedIn,
+    ifPremiumThenProceed,
+    async (req, res, next) => {
+        const chatid = req.params.id;
+        const user = await getTokenInformation(req, res);
+        try {
+            const chat = await Chat.findById(chatid);
+
+            if (!chat) {
+                res.status(401).json(returnResponse(true, "noSuchChat", "-"));
+
+                return;
+            }
+
+            if (chat.user != user.userid) {
+                res.status(402).json(returnResponse(true, "notYourChat", "-"));
+
+                return;
+            }
+
+            const usr = chat.user;
+            const doc = chat.doctor;
+            await Chat.findByIdAndDelete(chatid);
+            await User.findByIdAndUpdate(usr, {
+                $pull: {chats: chatid}``
+            });
+            await Doctor.findByIdAndUpdate(doc, {
+                $pull: {chats: chatid}
+            });
+
+            res.status(200).json(returnResponse(false, "deletedExistingChat", chat));
+
+            return;
+        } catch (error) {
+            res.status(403).json(returnResponse(true, "errorAtDMDelete", "-"));
 
             return;
         }
