3ac582753c3598a68897169587debda8e057ffe3
yhj9585 <85930226+yhj9585@users.noreply.github.com>
2025-01-26 17:51:09 +0900

    Add files via upload

--- frontend_doctor/lib/controllers/chat_controller.dart
+++ frontend_doctor/lib/controllers/chat_controller.dart
@@ -1,11 +1,6 @@
 import 'package:dio/dio.dart';
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:jwt_decoder/jwt_decoder.dart';
-import 'package:intl/intl.dart';
 
 import 'auth/auth_interceptor.dart';
 import '../model/chat_dart_model.dart';
